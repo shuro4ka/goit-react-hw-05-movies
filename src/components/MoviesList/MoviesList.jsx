69b507042d8a6c274bdd3509b68f6c646a8f9021
shuro4ka <zzlesia77@gmail.com>
2023-01-21 12:08:06 +0000

    a

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,12 +1,22 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import styles from './MoviesList.module.css';
 
 export const MoviesList = ({ moviesData, location }) => {
   return (
-    <ul>
+    <ul className={styles.moviesList}>
       {moviesData.map(({ original_title, name, id, poster_path }) => {
         return (
-          <li key={id}>
+          <li key={id} className={styles.listItem}>
+            {poster_path ? (
+              <img src={`https://image.tmdb.org/t/p/w400${poster_path}`} alt="Poster" />
+              ) : (
+              <img
+                className={styles.noImage}
+                src="https://icons.iconarchive.com/icons/icons8/windows-8/512/City-No-Camera-icon.png"
+                alt="No Poster Available"
+              />
+            )}
             <Link
               to={
                 location.pathname === '/'
@@ -14,8 +24,8 @@ export const MoviesList = ({ moviesData, location }) => {
                   : JSON.stringify(id)
               }
               state={{ from: location }}
-            >
-              {original_title ? original_title : name}
+              >
+              <h2 className={styles.movieTitle}>{original_title.length > 32 ? original_title.slice(0, 32) + '...' : original_title}</h2>
             </Link>
           </li>
         );
--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -1,19 +1,18 @@
-// import { useMemo } from 'react';
+
 import { useEffect, useState } from 'react';
 import { useSearchParams, useLocation } from 'react-router-dom';
 import { getSearchedMovies } from 'service/api';
 import { Searchbar } from 'components/Searchbar/Searchbar';
 import styles from './Movies.module.css';
 import { MoviesList } from 'components/MoviesList/MoviesList';
-// import { useMemo } from 'react';
+
 
 const Movies = () => {
   const location = useLocation();
   const [movies, setMovies] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
   const [isLoading, setIsLoading] = useState(false);
-  //const imageURL = 'https://image.tmdb.org/t/p/w185';
-
+ 
 
   const onSubmit = e => {
     e.preventDefault();
@@ -37,43 +36,11 @@ const Movies = () => {
     }, 200);
   }, [filterParam]);
 
-  // const visibleMovies = useMemo(() => {
-  //   return movies.filter(
-  //     movie =>
-  //       movie.hasOwnProperty('title') &&
-  //       movie.title.toLowerCase().includes(filterParam.toLowerCase())
-  //   );
-  // }, [filterParam, movies]);
-
   return (
-    
     <div className={styles.listContainer}>
       <Searchbar onSubmit={onSubmit} />
-      {isLoading }
-            {movies && (
-                <MoviesList moviesData={movies} location={location} />
-            )} 
-      {/* {visibleMovies.length > 0 && (
-        <ul className={styles.moviesList}>
-          {visibleMovies.map(movie => (
-            <li className={styles.listItem} key={movie.id}>
-              <nav to={`${movie.id}`} state={{ from: location }}>
-                {movie.poster_path ? (
-                  <img src={imageURL + movie.poster_path} alt="Poster" />
-                ) : (
-                  <img
-                    className={styles.noImage}
-                    src="https://icons.iconarchive.com/icons/icons8/windows-8/512/City-No-Camera-icon.png"
-                    alt="No Poster Available"
-                  />
-                )}
-
-                <h2 className={styles.movieTitle}>{movie.title.length > 13 ? movie.title.slice(0, 13) + '...' : movie.title}</h2>
-              </nav>
-            </li>
-          ))}
-        </ul>
-      )} */}
+      {isLoading}
+      {movies && <MoviesList moviesData={movies} location={location} />}
     </div>
   );
 };
